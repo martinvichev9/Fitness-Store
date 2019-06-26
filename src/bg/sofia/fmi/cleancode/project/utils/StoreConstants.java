4fb4d9d4b0ff6ead403612149cad3cc092288cfb
Martin <m_vichev97@abv.bg>
2019-06-26 15:50:40 +0300

    Add StoreConstant which contains constants used in a store

---
+++ src/bg/sofia/fmi/cleancode/project/utils/StoreConstants.java
@@ -0,0 +1,52 @@
+package bg.sofia.fmi.cleancode.project.utils;
+
+public class StoreConstants {
+
+    public static final int PROTEINS_COUNT = 50;
+    public static final int PROTEIN_BARS_COUNT = 200;
+    public static final int AMINOS_COUNT = 200;
+    public static final int BCAA_COUNT = 120;
+    public static final int TSHIRTS_COUNT = 500;
+    public static final int SHORTS_COUNT = 300;
+    public static final int LEGGINS_COUNT = 70;
+    public static final int SWEATSHIRTS_COUNT = 60;
+
+    public static final double PROTEIN_PRICE_ONE = 123.34;
+    public static final double PROTEIN_PRICE_TWO = 89.89;
+    public static final double PROTEIN_PRICE_THREE = 213.34;
+    public static final double PROTEIN_PRICE_FOUR = 65.41;
+
+    public static final int PROTEIN_QUANTITY_ONE = 2000;
+    public static final int PROTEIN_QUANTITY_TWO = 980;
+    public static final int PROTEIN_QUANTITY_THREE = 5000;
+    public static final int PROTEIN_QUANTITY_FOUR = 2200;
+
+    public static final double PROTEIN_BAR_PRICE_ONE = 4.65;
+    public static final double PROTEIN_BAR_PRICE_TWO = 2.23;
+    public static final double PROTEIN_BAR_PRICE_THREE = 4.20;
+    public static final double PROTEIN_BAR_PRICE_FOUR = 5;
+
+    public static final int PROTEIN_BAR_QUANTITY = 400;
+
+    public static final double AMINO_PRICE_ONE = 34.78;
+    public static final double AMINO_PRICE_TWO = 30.21;
+    public static final double AMINO_PRICE_THREE = 26.15;
+
+    public static final int AMINO_BCAA_QUANTITY_ONE = 600;
+    public static final int AMINO_BCAA_QUANTITY_TWO = 300;
+
+    public static final double BCAA_PRICE_ONE = 40.69;
+    public static final double BCAA_PRICE_TWO = 23.45;
+    public static final double BCAA_PRICE_THREE = 30.50;
+
+    public static final double LONSDALE_TSHIRT_PRICE = 22;
+    public static final double TAPOUT_TSHIRT_PRICE = 30;
+
+    public static final double LEGGINS_PRICE = 20;
+
+    public static final double SHORTS_PRICE = 15;
+
+    public static final double SWEATSHIRT_PRICE_ONE = 45;
+    public static final double SWEATSHIRT_PRICE_TWO = 55;
+
+}
