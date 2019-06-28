5e2a18b8155f50bbba0fdee7dee468c096d1d836
Martin <m_vichev97@abv.bg>
2019-06-28 23:26:36 +0300

    Update Validator class

--- src/bg/sofia/fmi/cleancode/project/utils/Validator.java
+++ src/bg/sofia/fmi/cleancode/project/utils/Validator.java
@@ -2,6 +2,8 @@ package bg.sofia.fmi.cleancode.project.utils;
 
 import bg.sofia.fmi.cleancode.project.enums.Command;
 
+import java.util.Set;
+
 public class Validator {
 
     public static final boolean isValidUserChoice(String userChoice) {
@@ -11,13 +13,29 @@ public class Validator {
         return userChoice.equals(Command.EXIT.getCommand()) ||
                 userChoice.equals(Command.SEARCH_BRAND.getCommand()) ||
                 userChoice.equals(Command.BUY_PRODUCT.getCommand()) ||
-                userChoice.equals(Command.SEARCH_PRODUCT.getCommand()) ||
-                userChoice.equals(Command.VIEW_PRODUCT.getCommand());
+                userChoice.equals(Command.SEARCH_PRODUCT.getCommand());
 
     }
 
     public static final boolean isYesOrNoUserInput(String userInput) {
         return userInput.toLowerCase().equals("yes") || userInput.toLowerCase().equals("no");
+    }
+
+    public static final boolean containsBrand(Set<String> brands, String brand) {
+        return brands.contains(brand);
+    }
+
+    public static final boolean isCorrectProductType(String productType) {
+
+        return productType.equals(ProductType.AMINO_ACIDS.toLowerCase()) ||
+                productType.equals(ProductType.BCAA.toLowerCase()) ||
+                productType.equals(ProductType.LEGGINS.toLowerCase()) ||
+                productType.equals(ProductType.PROTEIN.toLowerCase()) ||
+                productType.equals(ProductType.PROTEIN_BAR.toLowerCase()) ||
+                productType.equals(ProductType.SHORTS.toLowerCase()) ||
+                productType.equals(ProductType.SWEATSHIRT.toLowerCase()) ||
+                productType.equals(ProductType.T_SHIRT.toLowerCase());
+
     }
 
 }
